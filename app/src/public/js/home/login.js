447e98e0b561b8fef14860ec9a9556dc5e159e0c
신명근/(협력사) 책임연구원/HKMC고급형6세대AVN Project(mk1.shin@lgepartner.com) <mk1.shin@lge.com>
2021-08-25 17:17:07 +0900

    dom을 이용한 HTML 적용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    pasword = document.querySelector("#pasword"),
+    loginBtn = document.querySelector("button");
+
+  loginBtn.addEventListener("click", login);
+
+  function login() {
+    const req = {
+        id: id.value,
+        pasword: pasword.value,
+    };
+
+    console.log(req);
+  }
