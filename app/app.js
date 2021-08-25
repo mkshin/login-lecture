86090407c34c54175a19e317eed7e95d47d238a3
신명근/(협력사) 책임연구원/HKMC고급형6세대AVN Project(mk1.shin@lgepartner.com) <mk1.shin@lge.com>
2021-08-25 13:34:30 +0900

    first commit

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strictt";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use => 미들웨어 등록해 주는 메서드
+
+module.exports = app;
