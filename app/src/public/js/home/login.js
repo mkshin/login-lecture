8bc52561df98a941b0663ecf3ab2330afb7af803
bugshin <bugshin@gmail.com>
2021-08-25 22:32:23 +0900

    frontend login 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,5 +18,16 @@ const id = document.querySelector("#id"),
             "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.err(new Error("로그인 중 에러 발생"));
+        });
   }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -7,11 +7,31 @@ const output = {
     login: (req, res) => {
         res.render("home/login");
     },
-}
+};
+
+const users = {
+    id: ["mkshin", "나개발", "김팀장"],
+    pasword: ["1234", "1234", "123456"],
+};
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            pasword = req.body.pasword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.pasword[idx] === pasword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
     }
 }
 module.exports = {
