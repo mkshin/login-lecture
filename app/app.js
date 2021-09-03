7e78cea958e5c62528c105693739702b6bc061b6
bugshin <bugshin@gmail.com>
2021-09-03 14:40:55 +0900

    Add morgan function to leave log

--- app/app.js
+++ app/app.js
@@ -4,9 +4,13 @@
 const express = require("express");
 //const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+
 
 const app = express();
+dotenv.config();
+
+const accessLogStream = require("./src/config/log");
 
 // 라우팅
 const home = require("./src/routes/home");
@@ -18,6 +22,8 @@ app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
 //URL을 통해 전달되는 데이터
 app.use(express.urlencoded({ extended: true}));
+app.use(morgan("dev"));
+app.use(morgan("common", { stream: accessLogStream }));
 
 app.use("/", home); // use => 미들웨어 등록해 주는 메서드
 
